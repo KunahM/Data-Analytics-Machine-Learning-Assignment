d256d547933dbcd0b3fa5ff7bbed34908c7ed1f9
KunahM <kunah.manoharan@gmail.com>
2023-03-10 15:32:36 +0000

    Adam Assignment Part 2 Python code

---
+++ DA Assignment part 2 - 22260186 Adam.py
@@ -0,0 +1,353 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Planet four image classification
+
+# In[90]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import torch 
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+import torchvision.models as models
+import sklearn.metrics as metrics
+import tqdm
+
+from torch.utils.data import DataLoader
+from torchvision.datasets.folder import pil_loader
+from pathlib import Path
+from PIL import Image
+from torchvision.models import resnet50, ResNet50_Weights
+
+
+# Change the device to "cpu" if you want to train on a CPU instead of a GPU.
+
+# In[91]:
+
+
+device = 'cpu'
+
+
+# In[92]:
+
+
+data = str()
+
+
+# ## Dataset
+# 
+# Here we define a custom Dataset object for the Planet Four data. You can read more about this in the PyTorch documentation: https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
+
+# In[93]:
+
+
+class PlanetFourDataset(object):
+    def __init__(self, split='train', transform=None, loader=pil_loader):
+        super().__init__()
+        self.split = split
+        self.base_dir = 'C:/Users/Kunah/OneDrive/Documents/Dublin City University/Assignment/DA && ML/planetfour 2/planetfour'
+        self.image_dir = self.base_dir + '/' + split
+        self.labels_file = self.base_dir + '/' + (split + '.csv')
+        self.labels_df = pd.read_csv(self.labels_file)
+        self.transform = transform
+        self.loader = loader
+        
+    def __getitem__(self, index):
+        row = self.labels_df.iloc[index]
+        filename = self.image_dir + '/' + (row.tile_id + '.jpg')
+        fans = int(row.fans)
+        blotches = int(row.blotches)
+        image = self.loader(str(filename))
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, torch.tensor([fans, blotches], dtype=torch.float32)
+    
+    def __len__(self):
+        return len(self.labels_df)
+
+
+# ## Data augmentation
+# 
+# It is standard practice in deep learning to augment the training examples to prevent the network from overfitting. Here I use some standard augmentations such as randomly mirroring the images.
+
+# In[94]:
+
+
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5914, 0.5822, 0.5465), (0.3023, 0.2994, 0.3010))                  
+])
+
+valid_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5914, 0.5822, 0.5465), (0.3023, 0.2994, 0.3010)) 
+])
+
+
+# ## Data loaders
+# 
+# In PyTorch, the data loaders take care of spinning up threads to load batches of data into memory from the dataset object.
+
+# In[95]:
+
+
+train_set = PlanetFourDataset('train', transform=train_transform)
+valid_set = PlanetFourDataset('valid', transform=train_transform)
+
+train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
+valid_loader = DataLoader(valid_set, batch_size=64, shuffle=False)
+
+
+# ## Load a pretrained model
+# 
+# Here we'll use ResNet50 model that has been pretrained on ImageNet and replace the final layer with a new one suited to our problem.
+
+# In[96]:
+
+
+model = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+model.fc = nn.Linear(2048, 2)
+model.to(device);
+
+
+# ## Loss
+# 
+# Images can contain fans, blotches, both, or neither. You could treat this as a four class softmax problem, or two binary classification problems. Here I take the latter approach and use a binary cross entropy loss. 
+
+# In[97]:
+
+
+criterion = nn.BCEWithLogitsLoss()
+
+
+# ## Optimizer
+# 
+# Stochastic gradient descent with momentum
+
+# In[110]:
+
+
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+
+# ## Training and validation functions
+
+# In[111]:
+
+
+avg_train_losses = []
+avg_valid_losses = []
+valid_accuracies = []
+
+trainloss_list = []
+validloss_list = []
+
+def make_list(x,y):
+    trainloss_list.append(x)
+    validloss_list.append(y)
+
+def train_for_epoch(optimizer):
+    model.train()
+
+    train_losses = []
+
+    for batch, target in tqdm.tqdm(train_loader):
+
+        # data to GPU
+        batch = batch.to(device)
+        target = target.to(device)
+
+        # reset optimizer
+        optimizer.zero_grad()
+
+        # forward pass
+        predictions = model(batch)
+        #breakpoint()
+
+        # calculate loss
+        loss = criterion(predictions, target)
+
+        # backward pass
+        loss.backward()
+
+        # parameter update
+        optimizer.step()
+
+        # track loss
+        train_losses.append(float(loss.item()))
+
+    train_losses = np.array(train_losses)
+    return train_losses
+
+
+def validate():
+    model.eval()
+
+    valid_losses = []
+    y_true, y_prob = [], []
+
+    with torch.no_grad():
+        for batch, target in valid_loader:
+
+            # move data to the device
+            batch = batch.to(device)
+            target = target.to(device)
+
+            # make predictions
+            predictions = model(batch)
+
+            # calculate loss
+            loss = criterion(predictions, target)
+            
+            # logits -> probabilities
+            torch.sigmoid_(predictions)
+
+            # track losses and predictions
+            valid_losses.append(float(loss.item()))
+            y_true.extend(target.cpu().numpy())
+            y_prob.extend(predictions.cpu().numpy())
+            
+    y_true = np.array(y_true)
+    y_prob = np.array(y_prob)
+    y_pred = y_prob > 0.5
+    valid_losses = np.array(valid_losses)
+
+    # calculate validation accuracy from y_true and y_pred
+    fan_accuracy = metrics.accuracy_score(y_true[:,0], y_pred[:,0])
+    blotch_accuracy = metrics.accuracy_score(y_true[:,1], y_pred[:,1])
+    exact_accuracy = np.all(y_true == y_pred, axis=1).mean()
+
+    # calculate the mean validation loss
+    valid_loss = valid_losses.mean()
+
+    return valid_loss, fan_accuracy, blotch_accuracy, exact_accuracy
+
+
+def train(epochs, first_epoch=1):
+    for epoch in range(first_epoch, epochs+first_epoch):
+
+        # train
+        train_loss = train_for_epoch(optimizer)
+
+        # validation
+        valid_loss, fan_accuracy, blotch_accuracy, both_accuracy = validate()
+        print(f'[{epoch:02d}] train loss: {train_loss.mean():0.04f}  '
+              f'valid loss: {valid_loss:0.04f}  ',
+              f'fan acc: {fan_accuracy:0.04f}  ',
+              f'blotch acc: {blotch_accuracy:0.04f}  ',
+              f'both acc: {both_accuracy:0.04f}'
+        )
+        
+        make_list(train_loss.mean(),valid_loss)
+        
+        # update learning curves
+        avg_train_losses.append(train_loss.mean())
+        avg_valid_losses.append(valid_loss)
+        valid_accuracies.append((fan_accuracy, blotch_accuracy, both_accuracy))
+        
+print('trainloss_list = ', trainloss_list)
+print('validloss_list = ', validloss_list)
+        
+        # save checkpoint
+        #torch.save(model, f'checkpoints/baseline_{epoch:03d}.pkl')
+
+
+# ## Constant classifier accuracy
+# 
+# Evaluate how accurate would a $f(x) = \text{"most common class"}$ classifier be? 
+
+# In[112]:
+
+
+def constant_clf_accuracy():
+    y_true, y_pred = [], []
+    with torch.no_grad():
+        for _, target in valid_loader:
+            y_true.extend(target.cpu().numpy())
+            y_pred.extend(np.ones((target.shape[0], 2), dtype=np.float32))
+            
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+        
+    # calculate validation accuracy from y_true and y_pred
+    f = metrics.accuracy_score(y_true[:,0], y_pred[:,0])
+    b = metrics.accuracy_score(y_true[:,1], y_pred[:,1])
+    t = np.all(y_true == y_pred, axis=1).mean()
+    print(f'fan: {f}  blotch: {b}  both: {t}')
+
+
+# In[113]:
+
+
+constant_clf_accuracy()
+
+
+# ## Train the model
+# Call the ``train(n)`` function to train for ``n`` epochs.
+
+# In[140]:
+
+
+train(3)
+
+
+# In[142]:
+
+
+print(trainloss_list)
+
+
+# In[145]:
+
+
+print(validloss_list)
+epoch_list =[1,2,3,4,5,6,7,8]
+
+
+# In[146]:
+
+
+plt.plot(epoch_list,trainloss_list)
+plt.plot(epoch_list,validloss_list)
+plt.xticks(epoch_list)
+plt.xlabel("EPOCHS")
+plt.ylabel("Losses")
+plt.show()
+
+
+# In[114]:
+
+
+train(5)
+
+
+# In[127]:
+
+
+print(trainloss_list)
+
+
+# In[135]:
+
+
+print(validloss_list)
+epoch_list =[1,2,3,4,5,]
+
+
+# In[136]:
+
+
+plt.plot(epoch_list,trainloss_list)
+plt.plot(epoch_list,validloss_list)
+plt.xticks(epoch_list)
+plt.xlabel("EPOCHS")
+plt.ylabel("Losses")
+plt.show()
+
